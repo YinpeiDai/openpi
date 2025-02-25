fc71134b6c49310cffb90a6bb642f9462e257f15
YinpeiDai <daiyp@umich.edu>
2025-02-25 15:29:24 -0500

    add train script

---
+++ my_scripts/train_slurm_pi0_fast_libero.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=pi0_fast_libero_finetune
+#SBATCH --output=/home/daiyp/openpi/logs/pi0_fast_libero_finetune-%j.out
+#SBATCH --gres=gpu:4
+#SBATCH --time=12-00:00:00
+#SBATCH --account=chaijy2
+#SBATCH --partition=spgpu
+#SBATCH --mem-per-gpu=46G
+#SBATCH --cpus-per-task=10
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+
+source /home/daiyp/.bashrc
+cd /home/daiyp/openpi
+
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.97 /nfs/turbo/coe-chaijy/daiyp/micromamba_gl/envs/openpi/bin/python  scripts/train.py pi0_fast_libero --exp-name=pi0_fast_libero_finetune_bs32 --batch-size=32 --overwrite --fsdp_devices=4 --log-interval 20 --save-interval 5000
