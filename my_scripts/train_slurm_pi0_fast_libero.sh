949f4411200d4bcd90d2753c283e2664e2adc3cd
YinpeiDai <daiyp@umich.edu>
2025-03-04 23:11:16 -0500

    train_slurm sh

--- my_scripts/train_slurm_pi0_fast_libero.sh
+++ my_scripts/train_slurm_pi0_fast_libero.sh
@@ -18,4 +18,4 @@ cd /home/daiyp/openpi
 
 
 # run on my data
-XLA_PYTHON_CLIENT_MEM_FRACTION=0.97 /nfs/turbo/coe-chaijy/daiyp/micromamba_gl/envs/openpi/bin/python  scripts/train.py pi0_fast_libero --exp-name=large_crosshair_dynamic_default_color-bs32 --batch-size=32 --overwrite --fsdp_devices=4  --log-interval 20 --save-interval 5000 --num_train_steps 32000 --checkpoint_base_dir  /home/daiyp/openpi/runs/ckpts   --assets_base_dir /home/daiyp/openpi/runs/assets --lerobot_repo_id large_crosshair_dynamic_default_color
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.97 /nfs/turbo/coe-chaijy/daiyp/micromamba_gl/envs/openpi/bin/python  scripts/train.py pi0_fast_libero --exp-name=pi0fast_large_crosshair_dynamic_default_color --batch-size=32 --overwrite --fsdp_devices=4  --log-interval 20 --save-interval 5000 --num_train_steps 32000 --checkpoint_base_dir  /home/daiyp/openpi/runs/ckpts   --assets_base_dir /home/daiyp/openpi/runs/assets --lerobot_repo_id large_crosshair_dynamic_default_color
